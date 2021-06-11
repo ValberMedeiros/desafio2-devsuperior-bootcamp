70d1f404245565c40fedfca160bb9f387e212f90
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-06-11 13:30:17 -0300

    Create controller and service for cities.

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,36 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    private CityService service;
+
+    public CityController(CityService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CityDTO> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,42 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.exceptions.DataBaseException;
+import com.devsuperior.bds02.exceptions.EntityNotFoundException;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    private CityRepository repository;
+
+    public CityService(CityRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<CityDTO> findAll() {
+        List<City> entities = repository.findAll(Sort.by("name"));
+        return entities.stream()
+                    .map(CityDTO::new)
+                    .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException(String.format("City with id %d not found!", id));
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(String.format("the city with id %d cannot be deleted as it is in use", id));
+        }
+    }
+
+}
